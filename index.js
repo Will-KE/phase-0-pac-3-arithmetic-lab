8bff02146b57168b388ad2f216a573c8a5ca9b13
wilfred <wilfredkorir46@gmail.com>
2022-08-14 22:55:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add(){
+    return 
+}
+
+function subtract() {
+    return
+}
+
+function multiply() {
+    return
+}
+
+function divide() {
+    return
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    ++n;
+    return (n);
+}
+
+function decrement(n) {
+    --n;
+    return (n);
+}
+
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+    return
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
